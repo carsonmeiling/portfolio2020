48f64a423adc8925dc485db63f7288a618c94454
Carson Meiling <carmc93@gmail.com>
2020-05-28 16:06:31 -0600

    landing page

--- src/components/TopLanding.js
+++ src/components/TopLanding.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styles from '../styles/toplanding.css';
 
 const TopLanding = () => {
 
   return(
     <div className="topLanding">
-
+      <div className="links">
+        <a href='twitter'>Twitter</a>
+        <a href='instagram'>Insta</a>
+        <a href='github'>Github</a>
+      </div>
+      <div className='header'>
+        <h3>I'm Carson.</h3>
+        <h4>A Web Developer</h4>
+      </div>
     </div>
   )
 }
