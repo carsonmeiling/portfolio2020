9f59c76e108e3c4a76ead61756326fc27eceb955
Carson Meiling <carmc93@gmail.com>
2020-06-01 14:47:44 -0600

    about component

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../styles/about.css';
+
+const About = () => {
+
+  return(
+    <div className="about">
+      <div className='top-desc'>
+        <h2>I enjoy making clean simple web applicaions.</h2>
+      </div>
+      <div className='bottom=desc'>
+        <h2>Outside of work, I enjoy being active and having fun with my wife. We enjoy scuba diving, long boarding, skiing, card games, and movie nights. </h2>
+      </div>
+    </div>
+  )
+}
+
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import TopLanding from './TopLanding';
+import About from './About';
 
 const Home = () => {
 
   return(
     <div>
       <TopLanding />
+      <About />
 
     </div>
   )
