63bcd6ccad70a94082681de8721cb756922b6f9c
Carson Meiling <carmc93@gmail.com>
2020-06-02 10:29:58 -0600

    json file for projects

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import StartingProjects from '../data/projects.json'
+
+const Projects = () => {
+  const [projects, setProjects] = useState{StartingProjects};
+  return(
+
+  )
+}
+
+export default Projects;
