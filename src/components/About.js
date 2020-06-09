d682c94eea27d936b34d4df694d20ed6c5d8eb63
Carson Meiling <carmc93@gmail.com>
2020-06-09 12:33:19 -0600

    final tweaks

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,15 @@ const About = () => {
 
   return(
     <div className="about">
-      <div className='top-desc'>
-        <h2>I enjoy making clean and simple web applicaions.</h2>
+      <h2 className='about-header'>Who I Am</h2>
+      <div className='descriptions'>
+        <div className='top-desc'>
+          <p>I am a Utah Native with a background in finance, aviation, and business. In my spare time I enjoy skiing, scuba diving, and cooking.</p>
+        </div>
+        <div className='bottom-desc'>
+          <p>I made the leap to development, and have enjoyed the challenge ever since then. I enjoy making clean and simple websites. My language experience includes Ruby on Rails, React, Postgresql, and HTML. </p>
+        </div>
       </div>
-      {/* <div className='bottom=desc'>
-        <h2></h2>
-      </div> */}
     </div>
   )
 }
